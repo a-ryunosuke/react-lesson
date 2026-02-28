8ed5e478c83117335c80543fa13de2b10bdc0008
ryunosukearima <a.ryunosuke0125@gmail.com>
2026-02-28 16:18:25 +0900

    a

--- context/src/components/typeScript/Function.tsx
+++
@@ -1,9 +0,0 @@
-export const Function = () => {
-    const fn = (a: number, b: number) => a + b
-
-    return (
-        <div className="p-8 border-2">
-            {fn(4, 6)}
-        </div>
-    )
-}
