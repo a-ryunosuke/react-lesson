9289a0ac857fceef9f9b85a15358227cb6dba6c0
ryunosukearima <a.ryunosuke0125@gmail.com>
2026-03-06 11:17:00 +0900

    Promise

--- async-lesson/src/component/PromiseThen.tsx
+++ async-lesson/src/component/PromiseThen.tsx
@@ -1,14 +1,21 @@
 export const PromiseThen = () => {
+    // Promiseで新しくオブジェクトが作られる
     const sample = new Promise(function(resolve, reject) {
+        // 1秒後に処理を実行
         setTimeout(function() {
-            resolve()
+            resolve("成功しました")
+            reject("失敗しました")
         }, 1000)
     })
 
-    sample.then(function(value) {
-        console.log("Promise成功")
-    })
 
+    sample
+    // resolve(処理が成功)なら、thenの引数に
+    .then((value) => console.log(value))
+    // reject(処理が失敗)なら、catchの引数に
+    .catch((error) => console.log(error))
+
+    // 同期処理なので
     console.log("先に出力")
     return (
         <div></div>
