e04d3e17f0b0a45baf78f9a2c8447c1f354df6d7
ryunosukearima <a.ryunosuke0125@gmail.com>
2026-03-21 21:33:02 +0900

    PureFunc

--- react-Official-Site-lesson/src/components/PureFunction..tsx
+++ react-Official-Site-lesson/src/components/PureFunction..tsx
@@ -1,5 +1,13 @@
 export const PureFunction = () => {
+    const cups = [];
+    for (let i = 0; i <= 6; i++){
+        cups.push(<Cup key={i} gest={i} />)
+    }
+    return cups
+}
+
+const Cup = ({gest}) => {
     return (
-        <div></div>
+        <h2>#{gest}</h2>
     )
 }
